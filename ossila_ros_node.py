bcc7be684bb0891f13c294daec96493fc1ba3bbe
HussainMSajwani <hussainmsajwani@gmail.com>
2024-06-05 18:52:42 +0400

    avg filter

--- ossila_ros_node.py
+++ ossila_ros_node.py
@@ -2,16 +2,22 @@ import xtralien
 import time
 import rospy
 from std_msgs.msg import Float64
+from collections import deque
+from statistics import mean
+
 
 
 rospy.init_node('ossila', anonymous=True)
 
 
-ohm_pub = rospy.Publisher('/piezo', Float64)
+curr_pub = rospy.Publisher('/current', Float64)
+ohm_pub = rospy.Publisher('/resistance', Float64)
 # with xtralien.Device() as SMU:
 #     SMU.reset()
 SMU = xtralien.Device('/dev/ttyACM0')
 # SMU.smu1.set.osr(0)
+
+R_queue = deque(51*[0], maxlen=51)
 
 # SMU.reset()
 try:
@@ -27,9 +33,12 @@ try:
         i += 1
         v, c = SMU.smu1.oneshot(5)[0]
         # print(v, c*1e6, i)
+        c_ohm = v/(c*1e3)
+        R_queue.append(c_ohm)
+        ohm = mean(R_queue)
+        curr_pub.publish(Float64(c))
+        ohm_pub.publish(Float64(ohm))
 
-        ohm_pub.publish(Float64(c))
-        
 
             # print((time.time() - now) / 100, i)
             # now = time.time()
